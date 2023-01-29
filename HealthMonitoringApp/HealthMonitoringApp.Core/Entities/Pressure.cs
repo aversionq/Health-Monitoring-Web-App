0fbef34fad34b75f0596302164cd50da4101ca54
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-01-29 23:33:17 +0400

    Done Core layer

---
+++ HealthMonitoringApp/HealthMonitoringApp.Core/Entities/Pressure.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthMonitoringApp.Core.Entities
+{
+    public partial class Pressure
+    {
+        public Guid Id { get; set; }
+        public int Systolic { get; set; }
+        public int Diastolic { get; set; }
+        public DateTime Date { get; set; }
+        public string UserId { get; set; } = null!;
+    }
+}
