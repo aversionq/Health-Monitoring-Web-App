280df9cf55f56f76ea89f1f9808f87b0947e5657
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-01-30 19:06:42 +0400

    Add DTOs to business layer

---
+++ HealthMonitoringApp/HealthMonitoringApp.Business/DTOs/PressureDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringApp.Business.DTOs
+{
+    public class PressureDTO
+    {
+        public Guid Id { get; set; }
+        public int Systolic { get; set; }
+        public int Diastolic { get; set; }
+        public DateTime Date { get; set; }
+        public string UserId { get; set; }
+    }
+}
