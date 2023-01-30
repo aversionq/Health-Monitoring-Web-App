0ae23f5e6ca56feaab53e022be24f5a316d47188
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-01-30 19:06:15 +0400

    Add data layer in infrastructure level

---
+++ HealthMonitoringApp/HealthMonitoringApp.Data/DatabaseContext/HealthMonitoringDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using HealthMonitoringApp.Application.Interfaces;
+using HealthMonitoringApp.Core.Entities;
+using System.Reflection.Emit;
+
+namespace HealthMonitoringApp.Data.DatabaseContext
+{
+    public partial class HealthMonitoringDbContext : DbContext
+    {
+        public HealthMonitoringDbContext()
+        {
+        }
+
+        public HealthMonitoringDbContext(DbContextOptions<HealthMonitoringDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pressure> Pressures { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pressure>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
