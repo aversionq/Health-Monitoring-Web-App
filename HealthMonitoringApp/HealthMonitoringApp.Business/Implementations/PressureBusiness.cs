d0bb3308fc33f8464c597bbc05b7e16d1b07366c
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-01-30 19:07:08 +0400

    Done business layer in infrastructure level

---
+++ HealthMonitoringApp/HealthMonitoringApp.Business/Implementations/PressureBusiness.cs
@@ -0,0 +1,94 @@
+﻿using HealthMonitoringApp.Application.Interfaces;
+using HealthMonitoringApp.Business.DTOs;
+using HealthMonitoringApp.Business.Interfaces;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HealthMonitoringApp.Core.Entities;
+
+namespace HealthMonitoringApp.Business.Implementations
+{
+    public class PressureBusiness : IPressureBusiness
+    {
+        private IPressureRepository _repository;
+        private Mapper _pressureMapper;
+        private Mapper _pressureToAddMapper;
+
+        public PressureBusiness(IPressureRepository repo)
+        {
+            _repository = repo;
+            SetupMappers();
+        }
+
+        public async Task AddPressure(PressureToAddDTO pressure)
+        {
+            var pressureEntity = _pressureToAddMapper.Map<PressureToAddDTO, Pressure>(pressure);
+            await _repository.AddPressure(pressureEntity);
+        }
+
+        public async Task DeletePressure(Guid pressureId)
+        {
+            var pressureEntity = await _repository.GetPressureById(pressureId);
+            if (pressureEntity != null)
+            {
+                await _repository.DeletePressure(pressureEntity);
+            }
+            else
+            {
+                throw new Exception("Pressure with such id not found");
+            }
+        }
+
+        public async Task<PressureDTO> GetPressureById(Guid pressureId)
+        {
+            try
+            {
+                var pressureEntity = await _repository.GetPressureById(pressureId);
+                var pressureDTO = _pressureMapper.Map<Pressure, PressureDTO>(pressureEntity);
+                return pressureDTO;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Pressure with such id not found");
+            }
+        }
+
+        public async Task<IEnumerable<PressureDTO>> GetUserPressure(string userId)
+        {
+            try
+            {
+                var userPressureEntity = await _repository.GetUserPressure(userId);
+                var userPressureDTO = _pressureMapper.Map<IEnumerable<Pressure>, 
+                    IEnumerable<PressureDTO>>(userPressureEntity);
+                return userPressureDTO;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Pressure with such id not found");
+            }
+        }
+
+        public async Task UpdatePressure(PressureDTO pressure)
+        {
+            var pressureEntity = _pressureMapper.Map<PressureDTO, Pressure>(pressure);
+            await _repository.UpdatePressure(pressureEntity);
+        }
+
+        private void SetupMappers()
+        {
+            var pressureMapperConfig = new MapperConfiguration(
+                cfg => cfg.CreateMap<PressureDTO, Pressure>().ReverseMap()
+                );
+            _pressureMapper = new Mapper(pressureMapperConfig);
+
+            var pressureAddMapperCondfig = new MapperConfiguration(
+                cfg => cfg.CreateMap<PressureToAddDTO, Pressure>().ReverseMap()
+                );
+            _pressureToAddMapper = new Mapper(pressureAddMapperCondfig);
+        }
+    }
+}
