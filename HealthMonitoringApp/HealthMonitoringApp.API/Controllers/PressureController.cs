8f606f7ed7e8cc0b4a66baece3b9ad073fb14b64
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-01-30 19:07:34 +0400

    Done API layer

---
+++ HealthMonitoringApp/HealthMonitoringApp.API/Controllers/PressureController.cs
@@ -0,0 +1,90 @@
+﻿using HealthMonitoringApp.Business.DTOs;
+using HealthMonitoringApp.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthMonitoringApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PressureController : ControllerBase
+    {
+        private IPressureBusiness _pressureBusiness;
+
+        public PressureController(IPressureBusiness pressureBusiness)
+        {
+            _pressureBusiness = pressureBusiness;
+        }
+
+        [HttpGet]
+        [Route("getUserPressure")]
+        public async Task<ActionResult<List<PressureDTO>>> GetUserPressure(string userId)
+        {
+            try
+            {
+                var userPressure = await _pressureBusiness.GetUserPressure(userId);
+                return Ok(userPressure);
+            }
+            catch (Exception)
+            {
+                return NotFound("User pressure not found");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PressureDTO>> GetPressureById(Guid id)
+        {
+            try
+            {
+                var pressure = await _pressureBusiness.GetPressureById(id);
+                return pressure;
+            }
+            catch (Exception)
+            {
+                return NotFound("Pressure with such id not found");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddPressure([FromBody] PressureToAddDTO pressure)
+        {
+            try
+            {
+                await _pressureBusiness.AddPressure(pressure);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("Not able to add this resource");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdatePressure([FromBody] PressureDTO pressure)
+        {
+            try
+            {
+                await _pressureBusiness.UpdatePressure(pressure);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("Not able to update this resource");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemovePressure(Guid id)
+        {
+            try
+            {
+                await _pressureBusiness.DeletePressure(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("Not able to update this resource");
+            }
+        }
+    }
+}
--- HealthMonitoringApp/HealthMonitoringApp.API/Program.cs
+++ HealthMonitoringApp/HealthMonitoringApp.API/Program.cs
@@ -1,8 +1,23 @@
+using HealthMonitoringApp.Application.Interfaces;
+using HealthMonitoringApp.Business.Implementations;
+using HealthMonitoringApp.Business.Interfaces;
+using HealthMonitoringApp.Data.DatabaseContext;
+using HealthMonitoringApp.Data.Implementations;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<HealthMonitoringDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLConnection"))
+    );
+builder.Services.AddScoped<IPressureBusiness, PressureBusiness>();
+builder.Services.AddScoped<IPressureRepository, PressureRepository>();
+builder.Services.AddScoped<HealthMonitoringDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
