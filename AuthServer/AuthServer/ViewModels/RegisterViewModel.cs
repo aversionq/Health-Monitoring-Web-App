f63a1bd7310104abaf063d37a37aeca08eb651b4
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2023-02-01 21:41:40 +0400

    Add auth api controller

--- AuthServer/AuthServer/ViewModels/RegisterViewModel.cs
+++ AuthServer/AuthServer/ViewModels/RegisterViewModel.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AuthServer.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuthServer.ViewModels
 {
-    public class RegistrationViewModel
+    public class RegisterViewModel
     {
         [Required]
         [StringLength(70, ErrorMessage = "Max length of first name is 70 characters")]
@@ -12,9 +13,17 @@ namespace AuthServer.ViewModels
         public string LastName { get; set; }
         [Required]
         public DateTime DateOfBirth { get; set; }
-        [Required(ErrorMessage = "Username or Email is required")]
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Required]
+        public double Weight { get; set; }
+        [Required]
+        public double Height { get; set; }
+        [Required]
+        public GenderType.GenderTypes Gender { get; set; }
     }
 }
