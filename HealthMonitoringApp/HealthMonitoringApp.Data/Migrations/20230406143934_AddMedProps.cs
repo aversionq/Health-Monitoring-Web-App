a40d90dac78081e32c02d5563afe2aa085190d36
DESKTOP-NEMIBMN\arskz <arskzxc@gmail.com>
2023-04-07 20:58:33 +0400

    Add migrations

---
+++ HealthMonitoringApp/HealthMonitoringApp.Data/Migrations/20230406143934_AddMedProps.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthMonitoringApp.Data.Migrations
+{
+    public partial class AddMedProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pulse",
+                table: "Pressures");
+
+            migrationBuilder.CreateTable(
+                name: "BloodSugars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    BloodSugar = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodSugars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeartRates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    Pulse = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeartRates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BloodSugars");
+
+            migrationBuilder.DropTable(
+                name: "HeartRates");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pulse",
+                table: "Pressures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
