0d8e7380ef026e911b695393b0eabc7a5aa5fc88
DESKTOP-NEMIBMN\arskz <arskzxc@gmail.com>
2023-06-10 23:38:10 +0400

    Add chat service

---
+++ AuthServer/AuthServer/Interfaces/IChatService.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.DTOs;
+
+namespace AuthServer.Interfaces
+{
+    public interface IChatService
+    {
+        public Task<IEnumerable<ChatDTO>> GetUserChats(string userId);
+        public Task<IEnumerable<ChatMessageDTO>> GetUserChatMessages(Guid chatId);
+        public Task AddChat(ChatToAddDTO chatToAddDTO);
+        public Task AddMessage(ChatMessageToAddDTO chatMessageDTO);
+    }
+}
